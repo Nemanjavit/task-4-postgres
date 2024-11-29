e26b5f75f5ecf4da1e060b03f495f4ffdf9d4b43
Nemanjavit <vitnemanja3@gmail.com>
2024-11-29 17:48:59 -0600

    fix

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
+const { Pool } = require("pg");
 const express = require("express");
-const db = require("./database");
 const app = express();
 
 require("dotenv").config();
 
 app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL + "?sslmode=require",
+});
+
+pool.connect((err) => {
+  if (err) throw err;
+  console.log("Connect to PostgreSQL successfully!");
+});
 
 app.get("/users", async (req, res) => {
   try {
