1a098088a04fed52035e801dfbdac35c54604828
Nemanjavit <vitnemanja3@gmail.com>
2024-11-29 18:08:27 -0600

    fix query

--- server/index.js
+++ server/index.js
@@ -1,26 +1,27 @@
 const { Pool } = require("pg");
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 require("dotenv").config();
 
+app.use(cors());
 app.use(express.json());
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL + "?sslmode=require",
 });
 
-pool.connect((err) => {
-  if (err) throw err;
-  console.log("Connect to PostgreSQL successfully!");
-});
-
 app.get("/users", async (req, res) => {
+  const client = await pool.connect();
+
   try {
-    const { rows } = await db.query("select * from users");
+    const { rows } = await client.query("SELECT * FROM users");
     res.json({ msg: "Ok", data: rows });
   } catch (error) {
     res.json({ msg: error.msg });
+  } finally {
+    client.release();
   }
 });
 
