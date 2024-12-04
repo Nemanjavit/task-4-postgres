05bc56182502b5aa93440a7580d95c1f6ef5cd9a
Nemanjavit <vitnemanja3@gmail.com>
2024-12-04 15:44:59 -0600

    change callback uri

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,18 @@ import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const redirectUri =
+  process.env.REACT_APP_VERCEL_ENV === "production"
+    ? `https://${window.location.hostname}/callback`
+    : process.env.REACT_APP_AUTH0_CALLBACK_URL;
+
 root.render(
   <Auth0Provider
-    domain={process.env.REACT_APP_AUTH_DOMAIN}
-    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+    domain={process.env.REACT_APP_AUTH0_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
     authorizationParams={{
-      redirect_uri: window.location.origin,
+      redirect_uri: redirectUri,
     }}
   >
     <React.StrictMode>
